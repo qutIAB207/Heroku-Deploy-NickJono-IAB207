2d553343a0a5c94edfd63656aaa41f927db15f71
Nick Edwards <n10754431@gmail.com>
2021-06-13 16:18:22 +1000

    initial commit

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -6,3 +6,8 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('landing.html')
+
+
+@bp.route('/festival')
+def festival():
+    return render_template('festival.html')
