f8015c9ff5637651fe615bf6bdf95a768fd1cee6
Nick Edwards <n10754431@gmail.com>
2021-06-20 14:13:10 +1000

    Added database support for landing page and festival routes

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, render_template
+from projectfile.website.models import User, Festival, Review, Booking
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def landing():
-    return render_template('landing.html')
+    festivals = Festival.query.all()
+    return render_template('landing.html', festivals=festivals)
 
 
-@bp.route('/festival')
-def festival():
-    return render_template('festival.html')
+@bp.route('/festival/<festivalID>')
+def festival(festivalID):
+    festivals = Festival.query.filter_by(id=festival.festivalID).first()
+    return render_template('festival.html', festivals=festivals)
 
 
 @bp.route('/account')
