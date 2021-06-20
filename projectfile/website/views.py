f582270aa5c781b826db09803dcc5b7cce3c1307
Nick Edwards <n10754431@gmail.com>
2021-06-20 15:01:51 +1000

    initial commit

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from projectfile.website.models import User, Festival, Review, Booking
+from .models import User, Festival, Review, Booking
 
 bp = Blueprint('main', __name__)
 
